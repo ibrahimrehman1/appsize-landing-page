8e7dbaed98a30ab0d31357f0b9d18ee6bb67af3c
Ibrahim Rehman <ibrahimrehman0346@gmail.com>
2021-01-17 17:16:55 +0500

    Landing Page 2 Completed with Hamburger Icon

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
+let state2 = false;
 window.addEventListener("scroll", (e)=>{
     if (window.scrollY){
         document.querySelector(".move-top").classList.add("move-top-show");
         document.querySelector(".nav").classList.add("nav-color");
+        state2 = true;
 
     }else{
         document.querySelector(".move-top").classList.remove("move-top-show");
-        document.querySelector(".nav").classList.remove("nav-color")
+        document.querySelector(".nav").classList.remove("nav-color");
+        document.querySelector('#nav-links').classList.remove("links");
+        state2 = false;
 
 
     }
-
-    window.removeEventListener("scroll", ()=>console.log("Done!"));
 })
+
+let state = false;
+document.querySelector("#burgerIcon").addEventListener("click", (e)=>{
+    if (!state){
+        document.querySelector("#bar-1").classList.add("tilt-1");
+        document.querySelector("#bar-2").classList.add("tilt-2");
+        document.querySelector("#bar-3").classList.add("tilt-3");
+        document.querySelector(".nav").classList.add("nav-extend");
+        document.querySelector('#nav-links').classList.add("links");
+        state = true;
+    }else{
+        document.querySelector("#bar-1").classList.remove("tilt-1");
+        document.querySelector("#bar-2").classList.remove("tilt-2");
+        document.querySelector("#bar-3").classList.remove("tilt-3");
+        document.querySelector(".nav").classList.remove("nav-extend");
+        document.querySelector('#nav-links').classList.remove("links");
+        state = false;
+    }
+
+    if (!state2){
+        document.querySelector(".nav").classList.add("nav-color");
+    }
+
+
+})
+
+function minimize(){
+    if (state){
+        document.querySelector("#bar-1").classList.remove("tilt-1");
+        document.querySelector("#bar-2").classList.remove("tilt-2");
+        document.querySelector("#bar-3").classList.remove("tilt-3");
+        document.querySelector(".nav").classList.remove("nav-extend");
+        document.querySelector('#nav-links').classList.remove("links");
+        state = false;
+    }
+}
